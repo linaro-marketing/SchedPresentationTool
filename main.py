eb792a84a1ef4f1ebb21b7ce6208bc818883543f
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-03-21 10:00:39 +0000

    Latest updated to the sched presentation tool

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import json
 # Import the API_KEY variable
 # Your API_KEY can be fetched from the https://event_name.sched.com/editor/exports/api
 from secrets import SCHED_API_KEY
+from googlesheet import GoogleSheetAPI
 
 class SchedPresentationExportTool:
     
@@ -29,6 +30,8 @@ class SchedPresentationExportTool:
         self._download_location = connect_code + "/"
         # Blacklisted tracks to ignore when creating pages/resources.json
         self.blacklistedTracks =  ["Food & Beverage","Informational"]
+        # Googlesheet API Instance
+        self.googlesheet = GoogleSheetAPI(True)
         # Main Method
         self.main()
     
@@ -39,6 +42,8 @@ class SchedPresentationExportTool:
         # Get the results from sched api
         self.export_data = self.get_api_results("/api/session/export?api_key={0}&format=json&fields=event_key,event_type,active,name,id,files,speakers")
         # Download to pdfs/other files for each session in sched.com
+        self.users_data = self.get_api_results("/api/user/list?api_key={0}&format=json")
+        # Download for details pdfs/other files in each session in sched.com
         sessions_data = self.download_session_files(self.export_data)
         # Download/update the resources.json file
         self.update_resources_json_file(sessions_data)
@@ -91,6 +96,7 @@ class SchedPresentationExportTool:
         This method grabs the files/session_id's for each session and updates the resources.json file
         in s3.
         """
+        self.missing_presentations =  []
         sessions = []
         for session in export_data:
             
@@ -132,6 +138,7 @@ class SchedPresentationExportTool:
                     if session["files"]:
                         # Create a new other_files list for storing the non-pdf files
                         other_files = []
+                        found_pdf = False
                         # Loop over all the files for this session
                         for session_file in session["files"]:
                             # Check if the file name endswith .pdf
@@ -139,6 +146,7 @@ class SchedPresentationExportTool:
                                 # Set output folder to presentations/
                                 output_folder = "presentations/"
                                 output_filename = session_id.lower() + ".pdf"
+                                found_pdf = True
                             # File is not a pdf so add to the other files array
                             else:
                                 # Change the output folder for syncing to the other files folder on s3
@@ -152,12 +160,29 @@ class SchedPresentationExportTool:
 
                             # Download the file
                             downloaded = self.grab_file(session_id, session_file["path"], output_filename, output_folder)
+                        
+                        if found_pdf == False:
+                            try:
+                                speakers = session["speakers"]
+                            except Exception as e:
+                                speakers = None
+                            if speakers != None:
+                                speakers = self.getDetailedSpeakers(speakers)
+                            self.missing_presentations.append(
+                                [session_id, speakers])
 
                         # Set the other files key to the array of other files
                         resources_session_data["other_files"] = other_files
                     # Add to main list of sessions to update resources.json with.
                     sessions.append(resources_session_data)
                 except KeyError:
+                    try:
+                        speakers = session["speakers"]
+                    except Exception as e:
+                        speakers = None
+                    if speakers != None:
+                        speakers = self.getDetailedSpeakers(speakers)
+                    self.missing_presentations.append([session_id, speakers])
                     print("No files found for {}".format(session_id))
         
         print("Please sync the presentations folder using the command below:")
@@ -168,8 +193,21 @@ class SchedPresentationExportTool:
         print("aws s3 --profile connect-linaro-org-Owner cp other_files/ s3://connect.linaro.org/{}/other_files/ --recursive".format(self.connect_code))
         input("Press enter to continue...")
 
+        with open("missing_presentations.txt", "w") as missing_presentations_file:
+            for each in self.missing_presentations:
+                missing_presentations_file.write(str(each) + "\n")
+
         return sessions
 
+    def getDetailedSpeakers(self, speakers):
+        """
+        Gets detailed speakers array
+        """
+        for speaker in speakers:
+            for user in self.users_data:
+                if speaker["username"] == user["username"]:
+                    speaker["email"] = user["email"]
+        return speakers
     def grab_file(self, session_id, url, output_filename, output_path="speaker_images/"):
         """
         Fetches attendee photo from the pathable data
@@ -190,7 +228,16 @@ class SchedPresentationExportTool:
             print(e)
             image = False
             return False
+
+    def updateGooglesheet(self):
+        """ 
+        Updates a Googlesheet defined in googlesheet.py with a list of sessions that are
+        missing presentations
+        """
+        
+        self.googlesheet.updateGoogleSheet(self.missing_presentations)
         
 if __name__ == "__main__":
     
     presentations = SchedPresentationExportTool("https://bkk19.sched.com", "bkk19")
+    presentations.updateGooglesheet()
