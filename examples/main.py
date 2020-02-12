e137cbcdabab6c02c1b10ce9f983e7bb8f971ce8
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-02-12 09:10:19 +0000

    Updated example script with BUD20 params

--- examples/main.py
+++ examples/main.py
@@ -3,7 +3,8 @@ import sys
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
 from sched_presentation_tool import SchedPresentationTool
+from secret import SCHED_API_KEY
 
 if __name__ == "__main__":
     resources_updater = SchedPresentationTool(
-        "https://linaroconnectsandiego.sched.com", "san19", "apikey")
+        "https://bud20.sched.com", "bud20", SCHED_API_KEY)
